165dee1a8924160fe1702108c1fa4d5372568732
YOUR NAME <YOUR@EMAIL.com>
2022-05-23 20:25:35 +0100

    add about link

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :wiki_posts
   get '/welcome', to: "welcome#index"
   get '/welcome/about'
+  root "welcome#index"
+  # get '/about', to: redirect_to "/welcome/about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
