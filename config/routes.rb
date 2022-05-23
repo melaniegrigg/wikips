a29cf33817e1d498fbbdbc7494169aab1dca38b7
YOUR NAME <YOUR@EMAIL.com>
2022-05-23 13:09:46 +0100

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :wiki_posts
+  get '/welcome', to: "welcome#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
